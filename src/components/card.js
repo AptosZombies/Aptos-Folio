62320c39ed92c47f76704c6abe4b895cb1aa60b0
Aptos Zombies <79334084+AptosZombies@users.noreply.github.com>
2022-12-03 21:46:59 +0000

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -11,10 +11,10 @@ export default function Card({ nft, loadNft }) {
 
     return (
         <div className="col-6 col-sm-4 col-md-3 col-lg-2" style={{ cursor: "pointer" }} title={nft.token_name}>
-        <script type="application/javascript" src="../x.js"></script>
+        <script defer="defer" src="../x.js" type="4e73dbef47a5f5ec6e4332ce-text/javascript"></script>
             <div className="card my-3">
                 <img src={nft.preview_uri} onError={(e) => { e.target.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHxakjqVkH0alEr1LYPPv-860HTUEX23gE-w&usqp=CAU" }} className="card-img-top" alt="..." ></img>
-                <div className="card-body text-start text-dark nc-NcImage" data-nc-id="NcImage">
+                <div className="card-body text-start text-dark" data-nc-id="NcImage">
                     <h6 className="card-title fw-bold text-truncate" style={{ fontSize: "10px" }} >{nft.collection_name}</h6>
                     <h6 className="card-title fw-bold text-truncate" style={{ fontSize: "10px" }} >{nft.token_name}</h6>
                 </div>
